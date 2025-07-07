05fb4c2b518b8763bd9db65e2800df8d51bd6120
Bhuvanesh5-03 <santharajendran7@gmail.com>
2025-07-08 05:00:54 +0530

    Server error

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
 app.use(express.json());
-app.get('/Home',verifyToken,(res,req)=>{
+app.get('/Home',verifyToken,(req,res)=>{
     res.send({success:true});
 })
 app.post('/Signup', async (req, res) => {
